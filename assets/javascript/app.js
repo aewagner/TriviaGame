21a9aa5e23b7abe04ea4c05d9e661dbe8aa2387d
Austin Wagner <Austin@Austins-MacBook-Pro.local>
2017-09-20 12:50:11 -0700

    Questions and choices appear in html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,7 +16,7 @@ var timer = {
 
 	start: function() {
 		intervalId = setInterval(timer.count, 1000);
-		console.log("start")
+		console.log("start");
 		console.log(timer.time);
 
 		// if (decrement === 120) {
@@ -30,21 +30,74 @@ var timer = {
 		console.log("count");
 		console.log(timer.time);
 
-		if (decrement === 145) {
-			console.log("next question!")
+		// // if (decrement === 145) {
+		// // 	console.log("next question!")
+		// };
+
+		timer.newQuestion();
+
+	},
+
+	newQuestion: function () { 
+
+		$("#start-button").remove();
+
+		if (decrement === 146) {
+			console.log("Question 1");
+			$("#question").html("Question 1");
+			$("#choice-one").html("Choice A");
+			$("#choice-two").html("Choice B");
+			$("#choice-three").html("Choice C");
+			$("#choice-four").html("Choice D");
+			$("#choice-four").html("Choice E");
+
+		};
+
+		 if (decrement === 143) {
+			console.log("Question 2");
+			$("#question").html("Question 2")
+			$("#choice-one").html("Choice F");
+			$("#choice-two").html("Choice G");
+			$("#choice-three").html("Choice H");
+			$("#choice-four").html("Choice I");
+			$("#choice-four").html("Choice J");
+		};
+
+		if (decrement === 140) {
+			console.log("Question 3");
+			$("#question").html("Question 3")
+			$("#choice-one").html("Choice K");
+			$("#choice-two").html("Choice L");
+			$("#choice-three").html("Choice M");
+			$("#choice-four").html("Choice N");
+			$("#choice-four").html("Choice O");
+		};
+
+		 if (decrement === 137) {
+			console.log("Question 4");
+			$("#question").html("Question 4")
+			$("#choice-one").html("Choice P");
+			$("#choice-two").html("Choice Q");
+			$("#choice-three").html("Choice R");
+			$("#choice-four").html("Choice S");
+			$("#choice-four").html("Choice T");
+		};
+
+		if (decrement === 133) {
+			console.log("Question 5");
+			$("#choice-one").html("Choice U");
+			$("#choice-two").html("Choice V");
+			$("#choice-three").html("Choice X");
+			$("#choice-four").html("Choice Y");
+			$("#choice-four").html("Choice Z");
+
 		};
 
 	},
 
-	// newQuestion: function () { 
-
-	// 	if (decrement === 120) {
-	// 		console.log("next question!")
-	// 	};
-
-	// },
-
 };
+
+
 
 $("button").on("click", timer.start);
 $("button").on("click", timer.newQuestion);
